d98309b96aa10bd29fad52210f398dfaa7446c00
TorezhanMailybay <torezhanmailybai@gmail.com>
2026-06-09 11:28:47 +0500

    Add Practice1 Python basics examples

---
+++ P1/Variables.py
@@ -0,0 +1,34 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
